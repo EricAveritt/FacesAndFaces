545f1949d707bc2a8e66a2032e39af6338e0bd71
Eric Averitt <erictheguyjin@gmail.com>
2021-07-11 21:08:25 -0400

    Add project files.

---
+++ FacesWeb/WebMvc/RestClients/OrderManagementApi.cs
@@ -0,0 +1,54 @@
+﻿using WebMvc.ViewModels;
+using MassTransit.ConsumeConfigurators;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace  WebMvc.RestClients
+{
+    public class OrderManagementApi : IOrderManagementApi
+    {
+
+        private readonly IOrderManagementApi _restClient;
+        private readonly IOptions<AppSettings> _settings;
+        public OrderManagementApi(/*IConfiguration config,*/ HttpClient httpClient, IOptions<AppSettings> settings)
+        {
+            _settings = settings;
+            string apiHostAndPort = _settings.Value.OrdersApiUrl;
+                //config.GetSection("ApiServiceLocations").
+                //GetValue<string>("OrdersApiLocation");
+            httpClient.BaseAddress = new Uri($"{apiHostAndPort}/api");
+            _restClient = RestService.For<IOrderManagementApi>(httpClient);
+
+        }
+        public async Task<OrderViewModel> GetOrderById(Guid orderId)
+        {
+            try
+            {
+                return await _restClient.GetOrderById(orderId);
+            }
+            catch(ApiException ex)
+            {
+                if(ex.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task<List<OrderViewModel>> GetOrders()
+        {
+            return await _restClient.GetOrders();
+        }
+    }
+}
---
+++ Orders/OrdersApi/Persistence/OrdersContext.cs
@@ -0,0 +1,42 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using OrdersApi.Models;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersApi.Persistence
+{
+    public class OrdersContext:DbContext
+    {
+
+        public OrdersContext(DbContextOptions<OrdersContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            var converter = new EnumToStringConverter<Status>();
+            builder
+                .Entity<Order>()
+                .Property(p => p.Status)
+                .HasConversion(converter);
+                    
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+        public void MigrateDB()
+        {
+
+            // add this package to the project <PackageReference Include="Polly" Version="7.2.1" />
+            //and import the namespace Polly
+            Policy
+                .Handle<Exception>()
+                .WaitAndRetry(10, r => TimeSpan.FromSeconds(10))
+                .Execute(() => Database.Migrate());
+        }
+    }
+}
