5a74241d83a484012285b534013aa391124dd1d6
Eric Averitt <15947822+EricAveritt@users.noreply.github.com>
2021-07-13 21:36:15 -0400

    Cleaned up unnecessary using statements

--- FacesWeb/WebMvc/Startup.cs
+++ FacesWeb/WebMvc/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebMvc.RestClients;
 using WebMvc.Services;
 using MassTransit;
@@ -10,8 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.AzureAppServices;
 
 namespace WebMvc
 {
